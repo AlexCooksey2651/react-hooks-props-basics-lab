35bd06e8a937dcbe34c598d3b88059e5ea85ba96
Alex Cooksey <aecooksey2651@gmail.com>
2022-01-06 16:19:11 -0500

    commit tree 236cbce1d8df33dc1f8aacffc48044e9c6fe75b9

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,9 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio || props.bio === "" ? <p>{props.bio}</p> : null}
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links links={props.links} />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
